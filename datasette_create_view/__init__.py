a1d5ba99f2f598ad9126576e5a5acd65e5394e89
Simon Willison <swillison@gmail.com>
2024-03-06 22:48:36 -0500

    Initial working plugin, refs #1

---
+++ datasette_create_view/__init__.py
@@ -0,0 +1,102 @@
+from datasette import hookimpl, Response
+from datasette.utils import derive_named_parameters
+import urllib
+
+
+@hookimpl
+def query_actions(datasette, database, actor, query_name, sql):
+    if query_name:
+        return
+
+    async def inner():
+        db = datasette.get_database(database)
+        parameters = await derive_named_parameters(db, sql)
+        if parameters:
+            return
+        # User must have create-table permission
+        if not await datasette.permission_allowed(
+            actor, "create-table", resource=database
+        ):
+            return
+        return [
+            {
+                "href": datasette.urls.database(database)
+                + "/-/create-view?"
+                + urllib.parse.urlencode(
+                    {
+                        "sql": sql,
+                    }
+                ),
+                "label": "Create SQL view for this query",
+                "description": "A named SQL view provides a convenient alias for executing this query and using it in joins.",
+            },
+        ]
+
+    return inner
+
+
+async def create_view(request, datasette):
+    database = request.url_vars["database"]
+    db = datasette.get_database(database)
+    if request.method == "POST":
+        post_vars = await request.post_vars()
+        sql = (post_vars.get("sql") or "").strip()
+        name = (post_vars.get("name") or "").strip()
+        error_response = Response.redirect(
+            request.path
+            + "?"
+            + urllib.parse.urlencode(
+                {
+                    "sql": sql,
+                    "name": name,
+                }
+            )
+        )
+        if not await datasette.permission_allowed(
+            request.actor, "create-table", resource=database
+        ):
+            datasette.add_message(
+                request,
+                "create-table permission is needed to create views",
+                datasette.ERROR,
+            )
+            return error_response
+        if (not sql) or (not name):
+            datasette.add_message(
+                request, "SQL and name are both required", datasette.ERROR
+            )
+            return error_response
+        parameters = await derive_named_parameters(db, sql)
+        if parameters:
+            datasette.add_message(
+                request, "SQL query cannot have :named parameters", datasette.ERROR
+            )
+            return error_response
+        # Try to create the view
+        try:
+            await db.execute_write("create view {} as {}".format(name, sql))
+        except Exception as ex:
+            datasette.add_message(request, str(ex), datasette.ERROR)
+            return error_response
+        # View was created, redirect to it
+        datasette.add_message(
+            request, f"SQL view '{name}' has been created", datasette.INFO
+        )
+        return Response.redirect(datasette.urls.table(database, name))
+    return Response.html(
+        await datasette.render_template(
+            "create_view.html",
+            {
+                "sql": request.args.get("sql") or "",
+                "database": db.name,
+            },
+            request=request,
+        )
+    )
+
+
+@hookimpl
+def register_routes():
+    return [
+        (r"^/(?P<database>[^/]+)/-/create-view$", create_view),
+    ]
---
+++ tests/test_create_view.py
@@ -0,0 +1,98 @@
+from datasette.app import Datasette
+import pytest
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "actor,sql,should_see_option",
+    (
+        (None, "select 1", False),
+        ("non_root", "select 1", False),
+        ("root", "select 1", True),
+        ("root", "select :name", False),
+    ),
+)
+async def test_query_action_menu(actor, sql, should_see_option):
+    datasette = Datasette()
+    db = datasette.add_memory_database("test")
+    cookies = {}
+    if actor:
+        cookies = {"ds_actor": datasette.client.actor_cookie({"id": actor})}
+    response = await datasette.client.get("/test", params={"sql": sql}, cookies=cookies)
+    fragment = (
+        '<a href="/test/-/create-view?sql=select+1">Create SQL view for this query</a>'
+    )
+    if should_see_option:
+        assert fragment in response.text
+    else:
+        assert fragment not in response.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "actor,args,expected_error",
+    (
+        (
+            None,
+            {"name": "a", "sql": "select 1"},
+            "create-table permission is needed to create views",
+        ),
+        (
+            "non_root",
+            {"name": "a", "sql": "select 1"},
+            "create-table permission is needed to create views",
+        ),
+        ("root", {"name": "", "sql": "select 1"}, "SQL and name are both required"),
+        ("root", {"name": "a", "sql": ""}, "SQL and name are both required"),
+        (
+            "root",
+            {"name": "a", "sql": "select :id"},
+            "SQL query cannot have :named parameters",
+        ),
+    ),
+)
+async def test_create_view_errors(actor, args, expected_error):
+    # Get csfrtoken
+    datasette = Datasette()
+    db = datasette.add_memory_database("test")
+    cookies = {}
+    if actor:
+        cookies = {"ds_actor": datasette.client.actor_cookie({"id": actor})}
+    get_response = await datasette.client.get(
+        "/test/-/create-view", params=args, cookies=cookies
+    )
+    csrftoken = get_response.cookies["ds_csrftoken"]
+    cookies["ds_csrftoken"] = csrftoken
+    # Now do the POST
+    response = await datasette.client.post(
+        "/test/-/create-view", data=dict(args, csrftoken=csrftoken), cookies=cookies
+    )
+    assert response.status_code == 302
+    assert response.url == "http://localhost/test/-/create-view"
+    messages = datasette.unsign(response.cookies["ds_messages"], "messages")
+    assert messages == [[expected_error, 3]]
+
+
+@pytest.mark.asyncio
+async def test_happy_path():
+    datasette = Datasette()
+    db = datasette.add_memory_database("test")
+    cookies = {"ds_actor": datasette.client.actor_cookie({"id": "root"})}
+    get_response = await datasette.client.get("/test/-/create-view", cookies=cookies)
+    csrftoken = get_response.cookies["ds_csrftoken"]
+    cookies["ds_csrftoken"] = csrftoken
+
+    assert await db.view_names() == []
+
+    # Create the view
+    response = await datasette.client.post(
+        "/test/-/create-view",
+        data={"sql": "select 1", "name": "new_view", "csrftoken": csrftoken},
+        cookies=cookies,
+    )
+    assert response.status_code == 302
+    assert response.url == "http://localhost/test/-/create-view"
+    messages = datasette.unsign(response.cookies["ds_messages"], "messages")
+    assert messages == [["SQL view 'new_view' has been created", 1]]
+
+    assert await db.view_names() == ["new_view"]
